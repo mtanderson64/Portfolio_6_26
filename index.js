43b9d68217b45118e6471d72252fb34de06821fe
Mark Anderson <mtandereson64@gmail.com>
2026-06-26 06:32:17 -0500

    email modal progress

---
+++ index.js
@@ -0,0 +1,32 @@
+// template_xyb4feo
+// service_q8rzjit
+// public API key.    OWK-6DazwWjbaMWPP
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+        .sendForm(
+            'service_q8rzjit',
+            'template_xyb4feo',
+            event.target,
+            'OWK-6DazwWjbaMWPP'
+        ).then(() => {
+            setTimeout(() => {
+                loading.classList.remove("modal__overlay--visible")
+            }, 500)
+            setTimeout(() => {
+                success.classList += " modal__overlay--visible"
+            }, 10)
+        }).catch(() => {
+            loading.classList.remove('modal__overlay--visible')
+            alert(
+                "Email service unavailable. Contact directly at mtanderson64@gmail.com"
+            )
+        })
+            
+}
