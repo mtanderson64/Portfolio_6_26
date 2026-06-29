9907be34b026f32dd91f4c99704889c236d10da3
Mark Anderson <mtandereson64@gmail.com>
2026-06-29 07:03:05 -0500

    modal, dark mode, decoration progress

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+let isModalOpen = false
+let contrastToggle = false
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
+
 // template_xyb4feo
 // service_q8rzjit
 // public API key.    OWK-6DazwWjbaMWPP
+
+
+
 
 function contact(event) {
     event.preventDefault()
@@ -30,3 +48,18 @@ function contact(event) {
         })
             
 }
+
+
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = !isModalOpen
+        return document.body.classList.remove("modal--open")
+    }
+
+    isModalOpen = !isModalOpen
+    document.body.classList += " modal--open"
+
+    //console.log('toggleModal()')
+}
+
